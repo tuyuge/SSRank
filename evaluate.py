1c615dae2779d3cd14dcd3c06257f3c379f87b2c
tuyuge <120169151+tuyuge@users.noreply.github.com>
2023-03-10 15:12:41 +0800

    Add files via upload

--- evaluate.py
+++ evaluate.py
@@ -1,41 +1,31 @@
+from SSRank.utils import data_loader, get_noun_chunks
+from SSRank.select import get_keywords
+import os
+import multiprocessing
+from functools import partial
 import re
 from nltk.stem import PorterStemmer
-from get_candidates import get_all_nps
-import numpy as np
-import os
 
 ps = PorterStemmer()
+import copy
 
 
-def data_loader(index, dataset_name):
-    dataset_dir = 'data/{}'.format(dataset_name)
-    filename_list = os.listdir(os.path.join(dataset_dir, 'docsutf8'))
-
-    with open(os.path.join(dataset_dir, 'docsutf8', filename_list[index]), encoding="utf-8") as f:
-        text = f.read()
-    with open(os.path.join(dataset_dir, 'keys', filename_list[index][:-4] + '.key'), encoding="utf-8") as f:
-        keys = f.read()
-    data = {'text': text, 'keys': keys}
-    return data
+def save(i, dataset_dir, variable, value):
+    data = data_loader(i, dataset_dir)
+    try:
+        kwargs = {variable: value}
+        predictions = get_keywords(data, **kwargs)["keywords"].keys()
+    except:
+        predictions = get_noun_chunks(data["text"])
+    return '\n'.join(predictions)
 
 
-def get_stemmed_keys(keys, text):
-    stemmed = []
-    stemmed_text = ' '.join(ps.stem(word) for word in re.split(r"[ \-\n\t]+", text) if word)
-    for key in keys:
-        new_key = ' '.join(ps.stem(word) for word in re.split(r"[ \-\n\t]+", key) if word)
-        if new_key and new_key in stemmed_text and new_key not in stemmed:
-            stemmed.append(new_key)
-    return stemmed
-
-
-def get_stemmed_predictions(predictions):
-    stemmed_predictions = []
-    for prediction in predictions:
-        new_prediction = ' '.join(ps.stem(word) for word in re.split(r" |-", prediction) if word)
-        if new_prediction:
-            stemmed_predictions.append(new_prediction)
-    return stemmed_predictions
+def stemmed(phrases_list):
+    stemmed_list = []
+    for phrase in phrases_list:
+        new_phrase = ' '.join(ps.stem(word) for word in re.split(r"[ \-\t]+", phrase) if word)
+        stemmed_list.append(new_phrase)
+    return stemmed_list
 
 
 def partial_matching(pred, gold):
@@ -43,10 +33,12 @@ def partial_matching(pred, gold):
     for i in pred:
         if i in gold:
             common.append(i)
+            gold.remove(i)
         else:
             for j in gold:
                 if (i in j or j in i) and i not in common:
                     common.append(i)
+                    gold.remove(j)
     return common
 
 
@@ -54,64 +46,83 @@ def exact_matching(lst1, lst2):
     return [value for value in lst1 if value in lst2]
 
 
-class Evaluate:
-    def __init__(self, dataset_name="Inspec", partial_match=True, save=True):
-        self.dataset_name = dataset_name
-        self.partial_match = partial_match
-        self.save = save
-
-    def compute_metrics(self, i):
-        # compute result of a single doc
-        data = data_loader(i, self.dataset_name)
-        text = data["text"]
-        keys = data['keys'].split("\n")
-        predictions = get_all_nps(text)
-        stemmed_keys = get_stemmed_keys(keys, text)
-        stemmed_predictions = get_stemmed_predictions(predictions)
-
-        if self.partial_match:
-            common_keys = partial_matching(stemmed_keys, stemmed_predictions)
-        else:
-            common_keys = exact_matching(stemmed_keys, stemmed_predictions)
-
-        missed = [i for i in stemmed_keys if i not in common_keys]
-        result = f"Index:{str(i)}\nKeywords missed:\n{str(missed)}\nKeywords predicted:\n{str(common_keys)}\nOriginal text:\n{repr(text)}\nYour predictions:\n{predictions}\n"
-
-        if len(stemmed_keys) == 0 or stemmed_predictions == 0:
-            return {"index": i, "result": result, "metrics": (1, 1, 1)}
-        precision = len(common_keys) / len(stemmed_predictions)
-        recall = len(common_keys) / len(stemmed_keys)
-        if precision + recall > 0:
-            f1 = 2 * (precision * recall) / (precision + recall)
-        else:
-            f1 = 0
-        return {"result": result, "metrics": (precision, recall, f1)}
-
-    def evaluate(self, num_docs):
-        """Evaluate the result across a dataset"""
-        result_list = []
-        import concurrent.futures
-        if self.save:
-            f = open(f"result/{self.dataset_name}_{str(num_docs)}_result.txt", "w")
-            with concurrent.futures.ProcessPoolExecutor() as executor:
-                for item in executor.map(self.compute_metrics, range(num_docs)):
-                    f.write(item["result"])
-                    result_list.append(item["metrics"])
-            f.close()
-        else:
-            with concurrent.futures.ProcessPoolExecutor() as executor:
-                for item in executor.map(self.compute_metrics, range(num_docs)):
-                    result_list.append(item["metrics"])
-
-        mean_precision = np.mean([i[0] for i in result_list])
-        mean_recall = np.mean([i[1] for i in result_list])
-        mean_f1 = np.mean([i[2] for i in result_list])
-        with open(f"result/{self.dataset_name}_{str(num_docs)}_metrics.txt", "w") as f:
-            f.write(
-                f"Result of {self.dataset_name} at {str(num_docs)} docs:\n{mean_precision}, {mean_recall}, {mean_f1}")
+def merge_lists(l1):
+    l2 = []
+    for i in l1:
+        if i:
+            for j in i:
+                l2.append(j)
+    return l2
 
 
-# Testing
+def keys_loader(dataset_dir, dir_name, num):
+    keys_list = []
+    filename_list = os.listdir(os.path.join(dataset_dir, 'docsutf8'))
+    for i in range(num):
+        with open(os.path.join(dataset_dir, dir_name, filename_list[i][:-4] + '.key'), encoding="utf-8") as f:
+            keys = f.read().split("\n")
+        keys_list.append(stemmed(keys))
+    return keys_list
+
+
+def compute_metrics(keys_list, predictions_list, matching):
+    common_list = []
+
+    for i in range(len(predictions_list)):
+        keys_list_copy = copy.deepcopy(keys_list)
+        common_list.append(matching(predictions_list[i], keys_list_copy[i]))
+    new_common_list = merge_lists(common_list)
+    new_keys_list = merge_lists(keys_list)
+    new_predictions_list = merge_lists(predictions_list)
+
+    precision = len(new_common_list) / len(new_predictions_list)
+    recall = len(new_common_list) / len(new_keys_list)
+    f1 = 2 * precision * recall / (precision + recall)
+    return precision, recall, f1
+
+
+def compute_all_metrics(datasets, variables, num, match_method, data_dir):
+    metrics_dict = {}
+    mapping = {'partial_matching':partial_matching, 'exact_matching':exact_matching}
+    for dataset in datasets:
+        dataset_dir = f"{data_dir}/{dataset}"
+        keys_list = keys_loader(dataset_dir, "keys", num)
+        for variable in variables:
+            values = variables[variable]
+            for value in values:
+                new_dir = f"{dataset_dir}/{match_method}/{variable}={value}"
+                if not os.path.exists(new_dir):
+                    os.makedirs(new_dir)
+                new_save = partial(save, dataset_dir=dataset_dir, variable=variable, value=value)
+                pool = multiprocessing.Pool()
+                results = pool.map(new_save, range(num))
+                pool.close()
+                for j in range(num):
+                    with open(f"{new_dir}/{data_loader(j, dataset_dir)['file']}.key", "w", encoding="utf-8") as f:
+                        f.write(results[j])
+                print(f"{match_method} Results saved for {dataset} with {variable}={value}!")
+
+                dir_name = f"{match_method}/{variable}={value}"
+                predictions_list = keys_loader(dataset_dir, dir_name, num)
+                metrics = compute_metrics(keys_list, predictions_list, matching=mapping[match_method])
+                print(f"The {match_method} precision, recall and f1 score for {dataset} with {variable}={value} is {metrics}")
+                metrics_dict[(dataset, variable, value)] = metrics
+    return metrics_dict
+
+
+
+# Testing cases
 if __name__ == '__main__':
-    evaluate_case = Evaluate(dataset_name="Inspec", partial_match=True, save=True)
-    evaluate_case.evaluate(50)
+    datasets = ['Inspec', 'SemEval2017', 'KDD', 'WWW', 'JML']
+    variables = {'window_size': [2, 3, 4, 5, 6, 7, 8, 9, 10],
+                 'damping': [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.85, 0.9],
+                 'clustering': ['HAC_clustering', 'K_means']}
+    num = 200
+    match_method = 'partial_matching'
+    data_dir = f"D:/tyg_research/code 2.0/SSRank/data"
+    metrics_dict = compute_all_metrics(datasets, variables, num, match_method, data_dir)
+    import pandas as pd
+    df = pd.DataFrame(metrics_dict)
+    df.to_csv("all_metrics.csv")
+
+
